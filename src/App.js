c4bf0eef85368605d6667f0bc081cc795b95ce7f
Bùi Đức An <buiducan@Bui-uc-An.local>
2023-10-10 22:28:25 +0700

    Login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import Login from "./components/Login/Login";
+import Nav from "./components/Navigation/Nav";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world with React.js (Hoi Dan IT)
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="app-container">
+                <Nav />
+                <Switch>
+                    <Route path="/news">news</Route>
+                    <Route path="/about">about</Route>
+                    <Route path="/contact">contact</Route>
+                    <Route path="/login">
+                        <Login />
+                    </Route>
+                    <Route path="/" exact>
+                        Home
+                    </Route>
+                    <Route path="*">404 not found</Route>
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
